541e7754f9ae95cbd540a03cd107323a4c034d84
danielgp <danielgarduno282@gmail.com>
2025-09-25 16:07:36 -0600

    se resolvió el task2

---
+++ task2.js
@@ -0,0 +1,10 @@
+
+  const form = document.getElementById('form1');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const firstName = form.elements.fname.value;
+    const lastName = form.elements.lname.value;
+    console.log(`First Name: ${firstName}`);
+    console.log(`Last Name: ${lastName}`);
+  });
