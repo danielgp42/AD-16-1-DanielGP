063add061e876c55a040716ad88afe49312dffe8
danielgp <danielgarduno282@gmail.com>
2025-09-25 16:01:41 -0600

    se realizó el task1

---
+++ task1.js
@@ -0,0 +1,7 @@
+function js_style() {
+  const parrafo = document.getElementById('text');
+  parrafo.style.fontFamily = 'Comic Sans MS';
+  parrafo.style.fontSize = '24pt';
+  parrafo.style.color = 'blue';
+}
+  
